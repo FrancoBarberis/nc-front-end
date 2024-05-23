dd86bb80d9001b56cbf6d12a95bfedd472a2e9ce
FrancoBarberis <francobarberissic3@gmail.com>
2024-05-22 23:19:44 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        'sans': ['poppins'],
-        'serif': ['poppins'],
-        'mono': ['poppins'],
-        'display': ['poppins'],
-        'body': ['poppins'],
+      colors:{
+
+      },
+      
+      fontSize:{
       },
     },
   },
